d33c713470ef163438569baf6edbc408eb7e0814
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2023-04-06 22:51:08 +0800

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -2,7 +2,7 @@ function encrypt() {
   var input = document.getElementById("inputText").value;
   var key = "mdbylcgx"; // 用于AES加密的密钥
   const secret_key = CryptoJS.enc.Utf8.parse(key);
-  const iv = secret_key
+  const iv = secret_key;
   const encrypted = CryptoJS.DES.encrypt(input, secret_key, {
     iv: iv,
     padding: CryptoJS.pad.Pkcs7,
